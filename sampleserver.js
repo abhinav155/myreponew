1a6dfe328fed17bd5f21de4eb812dadfa5bdb905
abhinav156 <155abhinav@gmail.com>
2015-08-26 13:19:46 +0530

    error
    should build error

--- sampleserver.js
+++ sampleserver.js
@@ -2,10 +2,10 @@
 var http = require('http');
 
 
-function dealWithWebRequest(request, response) {
+f dealWithWebRequest(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("node,js");
-  response.end();
+  response.end()
 }
 
 var webserver = http.createServer(dealWithWebRequest).listen(8000,"127.0.0.1");
