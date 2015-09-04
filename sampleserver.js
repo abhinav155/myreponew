4630c73e7241ef3bbd99c1fe16073a05818cfab5
abhinav155 <abhinav.kumar@ritzysystems.com>
2015-09-04 12:55:50 +0530

    changed
    code updated for error checking

--- sampleserver.js
+++ sampleserver.js
@@ -13,5 +13,5 @@ var webserver = http.createServer(dealWithWebRequest).listen(8000,"127.0.0.1");
 webserver.once('listening', function() {
 
 	console.log('Server running at http://127.0.0.1:8000/');
-
+}
 });
